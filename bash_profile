c1da3bd05a63e5e62d3746e834fe1aa1d9db1805
Kazunori Sato <kazunori279@gmail.com>
2015-04-24 01:50:48 +0000

    Added bash_profile

---
+++ bash_profile
@@ -0,0 +1,7 @@
+
+# Go settings 
+
+export GOPATH=~/go
+export GOROOT=~/go
+export PATH=$PATH:/usr/local/go/bin
+
--- def_setup.sh
+++ def_setup.sh
@@ -6,7 +6,7 @@
 
 # install default tools
 sudo apt-get update
-sudo apt-get -y install tmux git golang curl 
+sudo apt-get -y install tmux git curl 
 
 # init git
 curl https://raw.githubusercontent.com/kazunori279/dotfiles/master/gitconfig -o ~/.gitconfig
@@ -15,6 +15,8 @@ git init
 
 # init go
 mkdir ~/go
-echo "export GOPATH=~/go" > ~/.bash_profile
+curl https://raw.githubusercontent.com/kazunori279/dotfiles/master/bash_profile -o ~/.bash_profile
 source ~/.bash_profile
+curl https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz -o /tmp/golang.tar.gz
+tar -C /usr/local -xzf /tmp/golang.tar.gz
 
