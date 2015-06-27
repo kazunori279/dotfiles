d7ddce8a17d475e725af4dc8021e3f17d781ee68
Kazunori Sato <kaz@sth.co.jp>
2015-06-27 18:32:25 +0900

    Update def_setup.sh

--- def_setup.sh
+++ def_setup.sh
@@ -6,7 +6,7 @@
 
 # install default tools
 sudo apt-get update
-sudo apt-get -y install tmux git curl build-essential python-dev
+sudo apt-get -y install tmux git curl build-essential python-dev python-setuptools
 
 # init git
 curl https://raw.githubusercontent.com/kazunori279/dotfiles/master/gitconfig -o ~/.gitconfig
